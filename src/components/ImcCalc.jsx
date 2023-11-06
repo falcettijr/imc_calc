53d5042375c46d146415e88d22e394f4d75515af
sergio falcetti <sergio.falcetti@codenapp.com>
2023-11-06 17:52:21 -0300

    create form

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -7,12 +7,17 @@ const ImcCalc = () => {
             <form id="imc-form">
                 <div className="form-inputs">
                     <div className="form-control">
-                        <label htmlFor="height">
-                            Altura:
-                        </label>
-                        <input type="text" />
+                        <label htmlFor="height">Altura:</label>
+                        <input type="text" name="height" id="height" placeholder="Exemplo 1,75" />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="weight">Peso:</label>
+                        <input type="text" name="weight" id="weight" placeholder="Exemplo 70,5" />
                     </div>
                 </div>
+                <div className="action-control"></div>
+                <button>Calcular</button>
+                <button>Limpar</button>
             </form>
         </div>
     );
